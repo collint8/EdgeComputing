0f73f90859aa53fd7a04aad7368b0fd0b4a0998e
fog <uouedgecomputing@gmail.com>
2019-08-05 13:27:30 -0600

    getting code to work on Linux still not encorporating mcast.

--- AggCode/nodeDetection.py
+++ AggCode/nodeDetection.py
@@ -9,23 +9,29 @@ import socket
 def run(routerIP):
 
 	#latency speed recording is possible with nmap.
-	nMappy =nmap.PortScanner()
+    nMappy =nmap.PortScanner()
 
 	#scanning the router's ip address, on specified ports.
-	nMappy.scan(hosts='192.168.0.1/24', arguments= '-n -sn ') #-sP -PE -PA21,23,24,80,3389
-	#scanner results are put into a matrix. Status is ignored, only the ip.
-	hosts_list = [(x) for x in nMappy.all_hosts()]
+    try: #Linux
+        nMappy.scan(hosts='192.168.0.0/24', arguments= '-n -sn ') #-sP -PE -PA21,23,24,80,3389
+    except: #Windows
+        nMappy.scan(hosts='192.168.0.1/24', arguments= '-n -sn ')
+    #scanner results are put into a matrix. Status is ignored, only the ip.
+    hosts_list = [(x) for x in nMappy.all_hosts()]
 			
 	#removing the router's IP address.
-	hosts_list.remove(routerIP)
+    hosts_list.remove(routerIP)
 
 	#identify aggregator IP address
-	aggIPList = socket.gethostbyname_ex(socket.gethostname())[2]
-	aggIP = aggIPList[-1]
+    try:#for Linux systems
+        aggIPList = socket.gethostbyname_ex(socket.gethostname()+".local")[2]
+    except: #for windows
+        aggIPList = socket.gethostbyname_ex(socket.gethostname())[2]
+    aggIP = aggIPList[-1]
 	#removing aggregator IP address
 
-	hosts_list.remove(aggIP)
-	return aggIP, hosts_list
+    hosts_list.remove(aggIP)
+    return aggIP, hosts_list
 
 print(run('192.168.0.1'))
 print(' ')
